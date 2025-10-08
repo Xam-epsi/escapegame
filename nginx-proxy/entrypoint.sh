6ee4cf224732f832e935ab5080aae3c8e336ed40
QueDlaFrappe <robin.armand1@ecoles-epsi.net>
2025-10-08 11:59:35 +0200

    docker

---
+++ nginx-proxy/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Vérifie si les certificats existent déjà
+if [ ! -f /etc/nginx/certs/fullchain.pem ] || [ ! -f /etc/nginx/certs/privkey.pem ]; then
+    echo "Génération des certificats auto-signés..."
+    openssl req -x509 -nodes -days 365 \
+        -newkey rsa:2048 \
+        -keyout /etc/nginx/certs/privkey.pem \
+        -out /etc/nginx/certs/fullchain.pem \
+        -subj "/C=FR/ST=IDF/L=Paris/O=Dev/CN=localhost"
+
+fi
+
+# Lancer Nginx
+exec "$@"
